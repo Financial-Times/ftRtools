e144eef96a9aaba5d508ecc3e611437699994f91
Oliver Elliott <oliver.elliott@ft.com>
2017-03-27 15:21:30 +0100

    tested_redshift_query

--- R/redshift_query.R
+++ R/redshift_query.R
@@ -1,8 +1,9 @@
 
 redshift_query <- function(sql_file, cluster, username, password){
-  query <- base::readLines(sql_file)
+  query <- base::paste(base::readLines(sql_file), collapse = '\n')
   connection <- ftRtools::redshift_connection(cluster, username, password)
   results <- RJDBC::dbGetQuery(connection, query)
   RJDBC::dbDisconnect(connection)
   results
 }
+
