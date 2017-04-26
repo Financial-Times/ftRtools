7e598c3ed04d9881f5bc0892767eb0c0114b84c5
Oli Elliott <oe3257@gmail.com>
2017-04-26 10:26:54 +0100

    add network and credentials info to redshift_query description

--- R/redshift_query.R
+++ R/redshift_query.R
@@ -1,7 +1,11 @@
 #' Query an FT Redshift cluster.
 #'
 #' \code{redshift_query} returns the output of a SQL query on one of the FT
-#' Redshift clusters.
+#' Redshift clusters. You must be connected to the FT internal network to use
+#' this function. If you are commiting code to  github, do not write your
+#' redshift user name and password into the file. Instead create a .Renviron
+#' file in your home directory and access the credentials via
+#' \code{Sys.getEnv()} (see examples).
 #'
 #' @param sql_file File name (and path) of a .sql you would like to run.
 #' @param cluster The name of the cluster on which the query should run. Use
@@ -9,6 +13,10 @@
 #' @param username Your Redshift username.
 #' @param password Your Redshift passowrd.
 #' @return A dataframe of the query output.
+#' @examples
+#' \dontrun{
+#' redshift_query("my_query.sql","int",Sys.getEnv("redshift_username"),Sys.getEnv("redshift_password"))
+#' }
 redshift_query <- function(sql_file, cluster, username, password){
   query <- base::paste(base::readLines(sql_file), collapse = '\n')
   connection <- ftRtools::redshift_connection(cluster, username, password)
