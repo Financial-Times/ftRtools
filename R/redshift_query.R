db97bfa9de85840f195691b0cd8eb295794da205
Oli Elliott <oe3257@gmail.com>
2017-04-26 09:24:19 +0100

    add redshift query title, description and parameter explanation

--- R/redshift_query.R
+++ R/redshift_query.R
@@ -1,4 +1,13 @@
-
+#' Query an FT Redshift cluster.
+#'
+#' \code{redshift_query} returns the output of a SQL query on one of the FT
+#' Redshift clusters.
+#'
+#' @param sql_file File name (and path) of a .sql you would like to run.
+#' @param cluster The name of the cluster on which the query should run. Use
+#'   'int' for the INT cluster and 'analytics' for the Analytics cluster.
+#' @param username Your Redshift username.
+#' @param password Your Redshift passowrd.
 redshift_query <- function(sql_file, cluster, username, password){
   query <- base::paste(base::readLines(sql_file), collapse = '\n')
   connection <- ftRtools::redshift_connection(cluster, username, password)
