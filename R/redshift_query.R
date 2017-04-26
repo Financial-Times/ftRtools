735f363015953b82b214cb68ee3d0bec1a5fc58d
Oli Elliott <oe3257@gmail.com>
2017-04-26 09:40:01 +0100

    add return info to redshift_query documentation

--- R/redshift_query.R
+++ R/redshift_query.R
@@ -8,6 +8,7 @@
 #'   'int' for the INT cluster and 'analytics' for the Analytics cluster.
 #' @param username Your Redshift username.
 #' @param password Your Redshift passowrd.
+#' @return A dataframe of the query output.
 redshift_query <- function(sql_file, cluster, username, password){
   query <- base::paste(base::readLines(sql_file), collapse = '\n')
   connection <- ftRtools::redshift_connection(cluster, username, password)
