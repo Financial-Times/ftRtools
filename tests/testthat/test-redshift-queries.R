38cb7aace63d650dc56ef10f756049c9d366281f
Oli Elliott <oliver.elliott@ft.com>
2018-03-09 11:12:22 +0000

    remove analytics tests and add from_file test that fails

--- tests/testthat/test-redshift-queries.R
+++ tests/testthat/test-redshift-queries.R
@@ -11,20 +11,30 @@ test_that("select queries run on int", {
   expect_gt(nrow(query_result), 0)
 })
 
-test_that("select queries run on analytics", {
-  query_result <- redshift_query("simple_query.sql",
-                                 "analytics",
-                                 Sys.getenv("redshift_username"),
-                                 Sys.getenv("redshift_password"))
-  expect_equal(is.data.frame(query_result), TRUE)
-  expect_gt(nrow(query_result), 0)
-})
+# test_that("select queries run on analytics", {
+#   query_result <- redshift_query("simple_query.sql",
+#                                  "analytics",
+#                                  Sys.getenv("redshift_username"),
+#                                  Sys.getenv("redshift_password"))
+#   expect_equal(is.data.frame(query_result), TRUE)
+#   expect_gt(nrow(query_result), 0)
+# })
 
 test_that("column names are preserved", {
   query_result <- redshift_query("simple_query.sql",
-                                 "analytics",
+                                 "int",
                                  Sys.getenv("redshift_username"),
                                  Sys.getenv("redshift_password"))
   expect_equal(names(query_result), c("tables"))
 })
 
+test_that("query as string runs on int", {
+  q <- readr::read_file(here::here("tests","testthat","simple_query.sql"))
+  query_result <- redshift_query(q,
+                                 "int",
+                                 Sys.getenv("redshift_username"),
+                                 Sys.getenv("redshift_password"),
+                                 from_file = FALSE)
+  expect_equal(is.data.frame(query_result), TRUE)
+  expect_gt(nrow(query_result), 0)
+})
